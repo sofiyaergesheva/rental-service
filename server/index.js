028bfc74d299de0b4954c5f3ef70842094b97055
Sofiya <sofiya.ergesheva@gmail.com>
2025-05-11 16:21:14 +0300

    feat: connecting to the PostgreSQL database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import sequelize from './config/database.js';
+
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
-app.listen(PORT, () => console.log(`Сервер запущен на порте ${PORT}`));
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`Сервер запущен на порте ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
