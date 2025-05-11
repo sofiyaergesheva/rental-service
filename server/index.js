e38b3b505bcd834f9d527e3328fdb7b1312199a7
Sofiya <sofiya.ergesheva@gmail.com>
2025-05-11 16:28:23 +0300

    feat: the first route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import sequelize from './config/database.js';
+import cors from 'cors';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 const start = async () => {
     try {
