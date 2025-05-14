e5e7c23c6d0e90877f3a49515a49f2f7d8316c7a
Sofiya <sofiya.ergesheva@gmail.com>
2025-05-14 21:40:51 +0300

    feat: working with files and registering a user

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import offerRouter from './offerRoutes.js';
+import userRouter from './userRoutes.js'
 
 const router = new Router();
 router.use('/', offerRouter);
+router.use('/', userRouter);
 
 export { router }
