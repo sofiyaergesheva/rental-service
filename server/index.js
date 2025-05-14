63b05a5f68ca63978ee6e599c7330f592365bc80
Sofiya <sofiya.ergesheva@gmail.com>
2025-05-14 21:17:16 +0300

    feat:error handling and middleware creation

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 import sequelize from './config/database.js';
 import cors from 'cors';
 import { router } from './routes/index.js';
+import errorMiddleware from './middleware/ErrorHandlingMiddleware.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/', router);
+app.use(errorMiddleware);
 
 const start = async () => {
     try {
