02005755917df8688d06759cca484e6b2032eb10
Sofiya <sofiya.ergesheva@gmail.com>
2025-05-18 16:53:07 +0300

    fix: changed config/database.js

--- server/config/database.js
+++ server/config/database.js
@@ -9,7 +9,14 @@ const sequelize = new Sequelize(
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+        logging: false
     }
 );
 
